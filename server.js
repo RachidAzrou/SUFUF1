c1cf2d609a43bc7bd49642e5116004586dd655dc
Rachid Azrou <rachid@Rachids-MacBook-Pro.local>
2025-02-12 12:38:01 +0100

    nu wel

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(Server draait op http://localhost:${PORT});
+  console.log(`Server draait op http://localhost:${PORT}`);
 });
