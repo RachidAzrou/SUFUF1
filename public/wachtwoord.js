9a673d571e0ffcb8e26fadbed28320ff013b7f5f
Rachid Azrou <rachid@Rachids-MacBook-Pro.local>
2025-02-13 11:58:53 +0100

    jj

--- public/wachtwoord.js
+++ public/wachtwoord.js
@@ -8,11 +8,18 @@ document.getElementById('wachtwoordForm').addEventListener('submit', function (e
     const urlParams = new URLSearchParams(window.location.search);
     const ruimte = urlParams.get('ruimte'); // Dit haalt de waarde op, bijvoorbeeld '1ste-verdieping' of 'garage'
 
+    // Log de waarde van de ruimte-parameter om te controleren of deze correct wordt opgehaald
+    console.log('Ruimte: ', ruimte);
+
     // Controleer het wachtwoord voor de juiste ruimte
     const wachtwoorden = {
         '1ste-verdieping': '1234',
         'garage': 'abcd'
     };
+
+    // Log de ingevoerde wachtwoord en het correcte wachtwoord om te zien of ze overeenkomen
+    console.log('Ingevoerd wachtwoord: ', wachtwoord);
+    console.log('Correct wachtwoord voor ruimte: ', wachtwoorden[ruimte]);
 
     if (wachtwoorden[ruimte] && wachtwoord === wachtwoorden[ruimte]) {
         // Wachtwoord klopt, stuur door naar het juiste vrijwilligersscherm
